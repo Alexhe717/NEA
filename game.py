c7a84c4c9fbb47cade9faabb90a5ef9d1a2f0569
Alex He <Alexhe717@gmail.com>
2025-09-08 15:48:19 +0100

    policy fucntion

--- game.py
+++ game.py
@@ -5,19 +5,19 @@ class Player:
 	def __init__(self,name,piece):
 		self.name=name
 		self.piece=piece
-	def move(self,x,y):
-		board.Board.change_state(x,y,self.piece)
+	def policy(self,board_dimension):
+		return random.randint(0,board_dimension-1),random.randint(0,board_dimension-1)
 class Game:
 	def __init__(self,board_dimension):
 		self.player_1=Player('1',1)
 		self.player_2=Player('2',2)
-		self.dimension=board_dimension
+		self.board_dimension=board_dimension
 		self.current_board=board.Board(board_dimension)
 	def make_move(self,turn_number):
 		if turn_number%2==0:#player 1
-			self.current_board.change_state(random.randint(0,self.dimension-1),random.randint(0,self.dimension-1),self.player_1.piece)
+			self.current_board.change_state(*self.player_1.policy(self.board_dimension),self.player_1.piece)
 		else:
-			self.current_board.change_state(random.randint(0,self.dimension-1),random.randint(0,self.dimension-1),self.player_2.piece)
+			self.current_board.change_state(*self.player_2.policy(self.board_dimension),self.player_2.piece)
 	def game_start(self):
 		self.turn_number=0
 		while True:
